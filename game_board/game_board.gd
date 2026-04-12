21b394847028912ec8522957d7e6f88161b6b77a
Ed Armstrong <ed@frar.ca>
2026-04-12 08:52:01 -0400

    checkin

--- game_board/game_board.gd
+++ game_board/game_board.gd
@@ -22,6 +22,15 @@ const TERRAIN_COUNTS := {
 	"desert":    1,
 }
 
+const VERTEX_OFFSETS := [
+	Vector2(0, -64),       # top
+	Vector2(55, -32),      # top-right
+	Vector2(55, 32),       # bottom-right
+	Vector2(0, 64),        # bottom
+	Vector2(-55, 32),      # bottom-left
+	Vector2(-55, -32),     # top-left
+]
+
 ## Catan board layout: ring of rows, each entry is column count.
 ## Pointy-top offset coords: rows 0-4, lengths 3,4,5,4,3.
 ## Col offsets per row to centre the board.
@@ -34,6 +43,15 @@ var _terrain_bag: Array[String] = []
 func _ready() -> void:
 	self._fill_terrain_bag()
 	self._place_tiles()
+
+
+## Returns the 6 world positions for the vertices of a given cell.
+func get_vertex_positions(cell: Vector2i) -> Array[Vector2]:
+	var center := self.map_to_local(cell)
+	var result: Array[Vector2] = []
+	for offset in VERTEX_OFFSETS:
+		result.append(center + offset)
+	return result
 
 
 func _fill_terrain_bag() -> void:
---
+++ game_board/structures.gd
@@ -0,0 +1,16 @@
+# structures.gd
+extends Node2D
+
+const SettlementScene := preload("res://settlement.tscn")
+const CityScene       := preload("res://city.tscn")
+const IndicatorScene  := preload("res://indicator.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
