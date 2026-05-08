3b071607067ea585617e0418140d3c2dae259c83
Ed Armstrong <ed@frar.ca>
2026-05-08 12:01:14 -0400

    changed number target layer to 1

--- game_board/game_board.gd
+++ game_board/game_board.gd
@@ -97,27 +97,6 @@ func _input(event: InputEvent) -> void:
 		)
 
 
-# func _input(event: InputEvent) -> void:
-# 	if not event is InputEventMouseButton: return
-# 	if event.button_index != MOUSE_BUTTON_LEFT: return
-# 	if not event.pressed: return
-
-# 	var mouse := self.get_global_mouse_position()
-# 	print("--- nodes at ", mouse, " ---")
-# 	self._print_nodes_at(mouse, self.get_tree().root)
-
-
-# func _print_nodes_at(pos: Vector2, node: Node) -> void:
-# 	if node is Sprite2D:
-# 		var sprite := node as Sprite2D
-# 		var local := sprite.to_local(pos)
-# 		var rect := sprite.get_rect()
-# 		if rect.has_point(local):
-# 			print("HIT: %s | global_pos: %s" % [sprite.get_parent().name, sprite.global_position])
-
-# 	for child in node.get_children():
-# 		self._print_nodes_at(pos, child)
-
 func _reset_view() -> void:
 	# clear targets
 	clear_targets_hnd()
