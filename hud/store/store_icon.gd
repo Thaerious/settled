b8bc83d3004d383e104ed080ed222b4ccd4b41c1
Ed Armstrong <ed@frar.ca>
2026-06-25 13:30:48 -0400

    working partial store dialog

--- hud/store/store_icon.gd
+++ hud/store/store_icon.gd
@@ -2,7 +2,7 @@ class_name StoreIcon
 extends PanelContainer
 
 @export var theme_state_map := ThemeStateMap.new()
-@onready var inner_border := self.find_child("InnerBorder", true, false) as HasThemeStateMap
+@onready var inner_border :HasThemeStateMap 
 
 
 @export var enabled := true:
@@ -11,41 +11,33 @@ extends PanelContainer
 	set(v):
 		enabled = v
 		if enabled:
-			var ib := self.inner_border as HasThemeStateMap
-			if ib:
-				ib.theme_state_map.apply(ib, ThemeStateMap.ThemeState.DEFAULT)
-				print("after apply: %s" % self.inner_border.theme_type_variation)	
+			self.theme_state_map.apply(self, ThemeStateMap.ThemeState.DEFAULT)
+			if self.inner_border:
+				self.inner_border.theme_state_map.apply(self.inner_border, ThemeStateMap.ThemeState.DEFAULT)		
 		else:
-			var ib := self.inner_border as HasThemeStateMap
-			if ib: 
-				ib.theme_state_map.apply(ib, ThemeStateMap.ThemeState.DISABLED)	
-				print("after apply: %s" % self.inner_border.theme_type_variation)	
+			self.theme_state_map.apply(self, ThemeStateMap.ThemeState.DISABLED)
+			if self.inner_border:
+				self.inner_border.theme_state_map.apply(self.inner_border, ThemeStateMap.ThemeState.DISABLED)
 
 			
 		
 
 
 func _ready() -> void:
-	self.enabled = self.enabled
 	self.mouse_entered.connect(self._mouse_entered)
 	self.mouse_exited.connect(self._mouse_exited)
-
-	print("A ", self.find_child("InnerBorder", true, false))
-	print("B ", self.find_child("InnerBorder", true, false) is HasThemeStateMap)
-
+	self.inner_border = self.find_child("InnerBorder", true, false) as HasThemeStateMap
+	self.enabled = self.enabled
 
 func _mouse_entered() -> void:	
 	if not self.enabled: return
 	self.theme_state_map.apply(self, ThemeStateMap.ThemeState.HOVER)
 	self.mouse_default_cursor_shape = CursorShape.CURSOR_POINTING_HAND
-
-	var ib := self.inner_border as HasThemeStateMap
-	if ib: ib.theme_state_map.apply(ib, ThemeStateMap.ThemeState.HOVER)	
+	self.inner_border.theme_state_map.apply(self.inner_border, ThemeStateMap.ThemeState.HOVER)	
 
 
 func _mouse_exited() -> void:
 	if not self.enabled: return
 	self.theme_state_map.apply(self, ThemeStateMap.ThemeState.DEFAULT)
 	self.mouse_default_cursor_shape = CursorShape.CURSOR_ARROW
-
 	self.inner_border.theme_state_map.apply(self.inner_border, ThemeStateMap.ThemeState.DEFAULT)	
