b7d83918816bfcfae1e4187b8bb209259af97de9
Ed Armstrong <ed@frar.ca>
2026-07-09 10:57:59 -0400

    hand dialog added

--- hud/hand_dialog/hand_dialog.gd
+++ hud/hand_dialog/hand_dialog.gd
@@ -6,32 +6,6 @@ var RESOURCE_QTY_LABEL_MAP: Dictionary[Model.ResourceTypes, Label] = {}
 var RESOURCE_EX_LABEL_MAP: Dictionary[Model.ResourceTypes, Label] = {}
 
 func _ready() -> void:
-	self.RESOURCE_QTY_LABEL_MAP = {
-		Model.ResourceTypes.BRICK: %QtyBrick,
-		Model.ResourceTypes.WOOD:  %QtyWood,
-		Model.ResourceTypes.ROCK:  %QtyRock,
-		Model.ResourceTypes.WHEAT: %QtyWheat,
-		Model.ResourceTypes.WOOL:  %QtyWool,
-	}
-
-	self.RESOURCE_EX_LABEL_MAP = {
-		Model.ResourceTypes.BRICK: %ExBrick,
-		Model.ResourceTypes.WOOD:  %ExWood,
-		Model.ResourceTypes.ROCK:  %ExRock,
-		Model.ResourceTypes.WHEAT: %ExWheat,
-		Model.ResourceTypes.WOOL:  %ExWool,
-	}
-
-	EventBus.resources_updated.connect(func(id: int, wallet: Wallet) -> void:
-		if id != Game.self_id: return
-		wallet.update_view(RESOURCE_QTY_LABEL_MAP)
-	)
-
-	EventBus.exchange_rate_set.connect(func(id: int, wallet: Wallet) -> void:
-		if id != Game.self_id: return
-		wallet.update_view(RESOURCE_EX_LABEL_MAP, "%s:1")	
-	)
-
 	EventBus.current_phase_updated.connect(self._on_current_phase_updated)
 	EventBus.model_loaded.connect(self._on_model_loaded)
 
